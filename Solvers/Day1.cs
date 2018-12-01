3fd14ff95d033f990ee93e460548ac4623bf4728
Mark Heath <mark.heath@gmail.com>
2018-12-01 07:48:29 +0000

    part 1

--- Solvers/Day1.cs
+++ Solvers/Day1.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Solvers
 {
     public class Day1
     {
+        public static int Part1Solver(string part1Input)
+        {
+            var total = 
+                Regex.Split(part1Input, "\r\n|\n")
+                            .Where(s => !String.IsNullOrEmpty(s))
+                           .Select(int.Parse)
+                           .Sum();
+            return total;
+        }
     }
 }
--- UnitTests/Day1Tests.cs
+++ UnitTests/Day1Tests.cs
@@ -1,18 +1,38 @@
 using NUnit.Framework;
+using Solvers;
+using System.IO;
+using System.Reflection;
 
 namespace Tests
 {
-    public class Tests
+    public class Day1Tests
     {
-        [SetUp]
-        public void Setup()
+
+        public static string GetInputFile()
         {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "UnitTests.day1.txt";
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+
+        [Test]
+        public void BasicAdding()
+        {
+            var total = Day1.Part1Solver("+5\r\n-2\r\n+4");
+            Assert.AreEqual(7, total);
         }
 
         [Test]
-        public void Test1()
+        public void Part1Solution()
         {
-            Assert.Pass();
+            var total = Day1.Part1Solver(GetInputFile());
+            Assert.AreEqual(445, total);
         }
     }
 }
