d6df944e16187d6352abfa5ad9d83efdd408cce4
Mark Heath <mark.heath@gmail.com>
2018-12-01 16:22:24 +0000

    refactoring tests

--- UnitTests/Day1Tests.cs
+++ UnitTests/Day1Tests.cs
@@ -1,33 +1,11 @@
 using NUnit.Framework;
 using Solvers;
-using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text.RegularExpressions;
 
 namespace Tests
 {
+
     public class Day1Tests
     {
-
-        public static string[] GetInputFile()
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceName = "UnitTests.day1.txt";
-
-            using (var stream = assembly.GetManifestResourceStream(resourceName))
-            using (var reader = new StreamReader(stream))
-            {
-                var input = reader.ReadToEnd();
-                return Regex.Split(input, "\r\n|\n")
-                       .Where(s => !string.IsNullOrEmpty(s))
-                       .ToArray();
-            }
-        }
-
-
-
         [Test]
         public void BasicAdding()
         {
@@ -38,7 +16,7 @@ namespace Tests
         [Test]
         public void Part1Solution()
         {
-            var total = Day1.Part1Solver(GetInputFile());
+            var total = Day1.Part1Solver(Helpers.GetInputLines(1));
             Assert.AreEqual(445, total);
         }
 
@@ -52,7 +30,7 @@ namespace Tests
         [Test]
         public void Part2Solution()
         {
-            var answer = Day1.Part2Solver(GetInputFile());
+            var answer = Day1.Part2Solver(Helpers.GetInputLines(1));
             Assert.AreEqual(219, answer);
         }
     }
---
+++ UnitTests/Helpers.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Tests
+{
+    static class Helpers
+    {
+        public static string[] GetInputLines(int day)
+        {
+            var input = GetEmbeddedResourceAsText($"UnitTests.day{day}.txt");
+            return Regex.Split(input, "\r\n|\n")
+                    .Where(s => !string.IsNullOrEmpty(s))
+                    .ToArray();
+        }
+
+        public static string GetEmbeddedResourceAsText(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
