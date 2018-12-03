5a5ab0eb50a78a2b48a98af4383c9f82ce59e912
Mark Heath <mark.heath@gmail.com>
2018-12-03 07:29:33 +0000

    Day 3 part 1

---
+++ Solvers/Day3.cs
@@ -0,0 +1,39 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Solvers
+{
+    public class Day3
+    {
+        public static int Part1Solver(string[] input)
+        {
+            var state = new int[1000, 1000];
+            var regex = new Regex(@"\#(\d+) \@ (\d+),(\d+)\: (\d+)x(\d+)");
+            var shapes = input.Select(s => regex.Match(s).Groups)
+                 .Select(g => new
+                 {
+                     n = int.Parse(g[1].Value),
+                     x = int.Parse(g[2].Value),
+                     y = int.Parse(g[3].Value),
+                     w = int.Parse(g[4].Value),
+                     h = int.Parse(g[5].Value)
+                 });
+            foreach(var shape in shapes)
+            {
+                for (var x = shape.x; x < shape.x + shape.w; x++)
+                    for (var y = shape.y; y < shape.y + shape.h; y++)
+                        state[x, y]++; //state[x, y] == 0 ? shape.n : -1;
+            }
+
+            int overlaps = 0;
+            for (var x = 0; x < 1000; x++)
+                for (var y = 0; y < 1000; y++)
+                    if (state[x,y] > 1) overlaps++;
+            return overlaps;
+        }
+    }
+}
---
+++ UnitTests/Day3Tests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using Solvers;
+
+namespace Tests
+{
+
+    public class Day3Tests
+    {
+
+        [Test]
+        public void Part1Solution()
+        {
+            var overlaps = Day3.Part1Solver(Helpers.GetInputLines(3));
+            Assert.AreEqual(115304, overlaps);
+        }
+
+        [Test]
+        public void Part1TestData()
+        {
+            var overlaps = Day3.Part1Solver("#1 @ 1,3: 4x4;#2 @ 3,1: 4x4;#3 @ 5,5: 2x2".Split(';'));
+            Assert.AreEqual(4, overlaps);
+        }
+
+
+    }
+}
