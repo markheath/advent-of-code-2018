31c3c04452bf250d7fc80e6c4de1107d4d9cab32
Mark Heath <mark.heath@gmail.com>
2018-12-05 07:59:25 +0000

    Day 5 part 1

---
+++ Solvers/Day5.cs
@@ -0,0 +1,48 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Solvers
+{
+    public static class Day5
+    {
+        public static int Part1Solver(string[] input)
+        {
+            var output = React(input[0]);
+            return output.Length;
+        }
+
+        private static string React(string input)
+        {
+            var sb = new StringBuilder();
+            var diff = Math.Abs('A' - 'a');
+            for(int n = 0; n < input.Length; n++)
+            {
+                if(sb.Length == 0)
+                {
+                    sb.Append(input[n]);
+                }
+                else if(Math.Abs(sb[sb.Length-1] - input[n]) == diff)
+                {
+                    sb.Length--;
+                }
+                else
+                {
+                    sb.Append(input[n]);
+                }
+            }
+            return sb.ToString();
+        }
+
+
+        public static int Part2Solver(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ UnitTests/Day5Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Solvers;
+
+namespace Tests
+{
+    public class Day5Tests
+    {
+        private const string testData = @"dabAcCaCBAcCcaDA";
+
+        [Test]
+        public void Part1TestData()
+        {
+            var polymerLength = Day5.Part1Solver(new[] { testData });
+            Assert.AreEqual(10, polymerLength);
+        }
+
+        [Test]
+        public void Part1Solution()
+        {
+            var polymerLength = Day5.Part1Solver(Helpers.GetInputLines(5));
+            Assert.AreEqual(9238, polymerLength);
+        }
+
+        
+    }
+
+}
