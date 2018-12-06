19ca8d6643283d1c032ced816ec5250d47d7a7ac
Mark Heath <mark.heath@gmail.com>
2018-12-06 23:06:13 +0000

    day 6 part 1

---
+++ Solvers/Day6.cs
@@ -0,0 +1,73 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Solvers
+{
+    public static class Day6
+    {
+        public static int Part1Solver(string[] input)
+        {
+            var coords = input.Select(x => x.Split(','))
+                .Select(c => (x: int.Parse(c[0]), y: int.Parse(c[1])))
+                .ToArray();
+            var minX = coords.Min(c => c.x);
+            var maxX = coords.Max(c => c.x);
+            var minY = coords.Min(c => c.y);
+            var maxY = coords.Max(c => c.y);
+
+            var edgePoints = new HashSet<int>();
+
+            var grid = new Dictionary<(int,int),Neighbour>();
+            for (var x = minX; x <= maxX; x++)
+            {
+                for (var y = minY; y <= maxY; y++)
+                {
+                    var neighbour = FindNearestNeighbour(x, y, coords);
+                    grid[(x, y)] = neighbour;
+                    if (x == minX || x == maxX || y == minY || y == maxY)
+                    {
+                        edgePoints.Add(neighbour.Point);
+                    }
+                }
+            }
+
+            // assume areas are all contiguous
+            return grid.Values
+                .Where(n => n.Point != -1)
+                .Where(n => !edgePoints.Contains(n.Point)
+                )
+                .CountBy(x => x.Point).Max(g => g.Value);
+        }
+
+        static Neighbour FindNearestNeighbour(int x, int y, (int,int)[] coords)
+        {
+            var nearest = coords.Select((c, n) => new Neighbour()
+            {
+                Point = n,
+                Distance = Math.Abs(x - c.Item1) + Math.Abs(y - c.Item2)
+            }).MinBy(n => n.Distance).ToArray();
+
+            if (nearest.Length == 1) return nearest[0];
+            return new Neighbour() { Point = -1, Distance = nearest[0].Distance };
+        }
+
+        [DebuggerDisplay("{Point}:{Distance}")]
+        struct Neighbour
+        {
+            public int Point { get; set; }
+            public int Distance { get; set; }
+            public override string ToString()
+            {
+                return $"{Point},{Distance}";
+            }
+        }
+
+        public static int Part2Solver(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/Day6Tests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Solvers;
+
+namespace Tests
+{
+    public class Day6Tests
+    {
+        const string testData = @"1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9";
+        [Test]
+        public void Part1TestData()
+        {
+            var result = Day6.Part1Solver(testData.Split("\r\n"));
+            Assert.AreEqual(17, result);
+        }
+
+        [Test]
+        public void Part1Solution()
+        {
+            var result = Day6.Part1Solver(Helpers.GetInputLines(6));
+            // not 8170 (too high)
+            Assert.AreEqual(3907, result);
+        }
+
+
+    }
+
+}
