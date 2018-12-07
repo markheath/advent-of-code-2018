5ce94d43979481185800d1bad64cbb2e299f9a69
Mark Heath <mark.heath@gmail.com>
2018-12-07 08:05:59 +0000

    Day7 part 1

---
+++ Solvers/Day7.cs
@@ -0,0 +1,54 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Solvers
+{
+    public static class Day7
+    {
+        public static string Part1Solver(string[] input)
+        {
+            var tree = new Dictionary<char, Links>();
+            foreach(var (pre,next) in input.Select(l => (l[5],l[36])))
+            {
+                if (!tree.ContainsKey(pre))
+                    tree[pre] = new Links();
+                if (!tree.ContainsKey(next))
+                    tree[next] = new Links();
+
+                tree[pre].Children.Add(next);
+                tree[next].Parents.Add(pre);
+            }
+
+            
+            var visited = new List<char>();
+
+            while (true)
+            {
+                var rootNode = tree.Where(n => n.Value.Parents.Count == 0).OrderBy(n => n.Key).FirstOrDefault();
+                if (rootNode.Key == 0) break;
+                visited.Add(rootNode.Key);
+                tree.Remove(rootNode.Key);
+                foreach (var n in tree.Values)
+                {
+                    n.Parents.Remove(rootNode.Key);
+                }
+
+            }
+            return visited.ToDelimitedString("");
+        }
+
+        class Links
+        {
+            public HashSet<char> Parents { get;  } = new HashSet<char>();
+            public HashSet<char> Children { get; } = new HashSet<char>();
+        }
+
+        public static int Part2Solver(string[] input)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ UnitTests/Day7Tests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Solvers;
+
+namespace Tests
+{
+    public class Day7Tests
+    {
+        const string testData = @"Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.";
+        [Test]
+        public void Part1TestData()
+        {
+            var result = Day7.Part1Solver(testData.Split("\r\n"));
+            Assert.AreEqual("CABDFE", result);
+        }
+
+        [Test]
+        public void Part1Solution()
+        {
+            var result = Day7.Part1Solver(Helpers.GetInputLines(7));
+            // not 8170 (too high)
+            Assert.AreEqual(3907, result);
+        }
+
+    }
+
+}
