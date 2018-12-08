a5111ced4e34bbb7a357a4eb2b29c3b2e35b3182
Mark Heath <mark.heath@gmail.com>
2018-12-08 17:54:20 +0000

    Day 8 part 1 solution

---
+++ Solvers/Day8.cs
@@ -0,0 +1,58 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solvers
+{
+    public static class Day8
+    {
+        public class Node
+        {
+            public List<Node> ChildNodes { get; } = new List<Node>();
+            public List<int> MetaData { get; } = new List<int>();
+            public int MetaDataCount { get; set; }
+            public int ChildNodeCount { get; set; }
+        }
+
+        public static IEnumerable<Node> ParseTree(IEnumerator<int> numbers, Node parent)
+        {
+            var newNode = new Node();
+            numbers.MoveNext();
+            newNode.ChildNodeCount = numbers.Current;
+            numbers.MoveNext();
+            newNode.MetaDataCount = numbers.Current;
+            if (parent != null)
+            {
+                parent.ChildNodes.Add(newNode);
+            }
+            yield return newNode;
+            for(int child = 0; child < newNode.ChildNodeCount; child++)
+            {
+                foreach (var children in ParseTree(numbers, newNode))
+                {
+                    yield return children;
+                }
+            }
+            for (int meta = 0; meta < newNode.MetaDataCount; meta++)
+            {
+                numbers.MoveNext();
+                newNode.MetaData.Add(numbers.Current);
+            }
+
+        }
+
+        public static int Part1Solver(string[] input)
+        {
+            var numbers = input[0].Split(' ').Select(int.Parse).GetEnumerator();
+            var nodes = ParseTree(numbers, null).ToList();
+            return nodes.Sum(n => n.MetaData.Sum());
+        }
+
+
+        public static int Part2Solver(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/Day8Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Solvers;
+
+namespace Tests
+{
+    public class Day8Tests
+    {
+        const string testData = @"2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+        [Test]
+        public void Part1TestData()
+        {
+            var result = Day8.Part1Solver(testData.Split("\r\n"));
+            Assert.AreEqual(138, result);
+        }
+
+        [Test]
+        public void Part1Solution()
+        {
+            var result = Day8.Part1Solver(Helpers.GetInputLines(8));
+            Assert.AreEqual(49426, result);
+        }
+
+
+    }
+}
