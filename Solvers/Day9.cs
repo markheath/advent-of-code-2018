cf0c7fccfbe023722a9157a4be00ecd70bf19e9f
Mark Heath <mark.heath@gmail.com>
2018-12-09 09:21:39 +0000

    Day 1 part 1 solution

---
+++ Solvers/Day9.cs
@@ -0,0 +1,67 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Solvers
+{
+    public static class Day9
+    {
+        class Position
+        {
+            public int MarbleNumber { get; set; }
+            public Position Left { get; set; }
+            public Position Right { get; set; }
+
+        }
+
+        // n.b. I'm sure high score can be solved without actually playing the game!
+        public static int PlayGame(int players, int lastMarble)
+        {
+            var scores = new int[players];
+            var currentBoard = new Position();
+            currentBoard.Left = currentBoard;
+            currentBoard.Right = currentBoard;
+            int currentPlayer = 0;
+            for(var currentMarble = 1; currentMarble <= lastMarble; currentMarble++)
+            {
+                if (currentMarble % 23 == 0)
+                {
+                    scores[currentPlayer] += currentMarble;
+                    var toRemove = currentBoard.Left.Left.Left.Left.Left.Left.Left;
+                    scores[currentPlayer] += toRemove.MarbleNumber;
+                    currentBoard = toRemove.Right;
+                    toRemove.Left.Right = currentBoard;
+                    currentBoard.Left = toRemove.Left;
+                }
+                else
+                {
+                    // regular insert
+                    var placeAfter = currentBoard.Right;
+                    var newMarble = new Position() { MarbleNumber = currentMarble, Left = placeAfter, Right = placeAfter.Right };
+                    placeAfter.Right = newMarble;
+                    newMarble.Right.Left = newMarble;
+                    currentBoard = newMarble;
+                }
+                currentPlayer++;
+                if (currentPlayer == players) currentPlayer = 0;
+            }
+
+            return scores.Max();
+        }
+    
+        public static int Part1Solver(string[] input)
+        {
+            var matches = Regex.Matches(input[0], @"\d+");
+            var players = int.Parse(matches[0].Value);
+            var lastMarble = int.Parse(matches[1].Value);
+            return PlayGame(players, lastMarble);
+        }
+
+        public static int Part2Solver(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/Day9Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Solvers;
+using System;
+
+namespace Tests
+{
+    public class Day9Tests
+    {
+        [TestCase(9, 25, 32)]
+        [TestCase(10, 1618, 8317)]
+        [TestCase(13, 7999, 146373)]
+        [TestCase(17, 1104, 2764)]
+        [TestCase(21, 6111, 54718)]
+        [TestCase(30, 5807, 37305)]
+        public void Part1TestData(int players, int lastMarble, int highScore)
+        {
+            var result = Day9.PlayGame(players, lastMarble);
+            Assert.AreEqual(highScore, result);
+        }
+
+        [Test]
+        public void Part1Solution()
+        {
+            var result = Day9.Part1Solver(Helpers.GetInputLines(9));
+            Assert.AreEqual(399645, result);
+        }
+
+
+    }
+}
