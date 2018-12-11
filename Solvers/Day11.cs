399fbc8ece2cf5e2a2e459cf69784bfbee806a22
Mark Heath <mark.heath@gmail.com>
2018-12-11 07:51:01 +0000

    Day 11 part 1

---
+++ Solvers/Day11.cs
@@ -0,0 +1,55 @@
+﻿using MoreLinq;
+using System;
+using System.Linq;
+
+namespace Solvers
+{
+    public static class Day11
+    {    
+        public static string Part1Solver(string[] input)
+        {
+            var serialNumber = int.Parse(input[0]);
+            var grid = CreateGrid(300, 300, serialNumber);
+            var (x,y) = FindLargestSubgrid(grid, 300, 300);
+            return $"{x},{y}";
+        }
+
+        public static (int,int) FindLargestSubgrid(int[,] grid, int width, int height)
+        {
+            var maxSubgrid = Enumerable.Range(0, width - 2)
+                .SelectMany(x => Enumerable.Range(0, height - 2).Select(y => (x, y)))
+                .Select(c => new
+                {
+                    Coord = c,
+                    Power = grid[c.x, c.y] + grid[c.x + 1, c.y] + grid[c.x + 2, c.y] +
+                grid[c.x, c.y + 1] + grid[c.x + 1, c.y + 1] + grid[c.x + 2, c.y + 1] +
+                grid[c.x, c.y + 2] + grid[c.x + 1, c.y + 2] + grid[c.x + 2, c.y + 2]
+                }).MaxBy(s => s.Power).Single();
+            return (maxSubgrid.Coord.x + 1, maxSubgrid.Coord.y + 1);
+        }
+
+        public static int[,] CreateGrid(int width, int height, int gridSerialNumber)
+        {
+            var grid = new int[width, height];
+            for (var x = 0; x < width; x++)
+                for (var y = 0; y < height; y++)
+                    grid[x, y] = GetPowerLevel(x+1, y+1, gridSerialNumber);
+            return grid;
+        }
+
+        public static int GetPowerLevel(int x, int y, int gridSerialNumber)
+        {
+            var rackId = x + 10;
+            var powerLevel = rackId * y;
+            powerLevel += gridSerialNumber;
+            powerLevel *= rackId;
+            powerLevel /= 100;
+            return powerLevel % 10 - 5;
+        }
+
+        public static int Part2Solver(string[] input)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ UnitTests/Day11Tests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Solvers;
+using System;
+
+namespace Tests
+{
+    public class Day11Tests
+    {
+        [Test]
+        public void Part1Solution()
+        {
+            var result = Day11.Part1Solver(new[] { "6042" });
+            Assert.AreEqual("21,61", result);
+        }
+
+        [TestCase(3, 5, 8, 4)]
+        [TestCase(122, 79, 57, -5)]
+        [TestCase(217, 196, 39, 0)]
+        [TestCase(101, 153, 71, 4)]
+        public void CanCalculatePowerLevel(int x, int y, int gridSerialNumber, int expected)
+        {
+            var powerLevel = Day11.GetPowerLevel(x,y,gridSerialNumber);
+            Assert.AreEqual(expected, powerLevel);
+        }
+
+        [TestCase(18, 29, "33,45")]
+        [TestCase(42, 30, "21,61")]
+        public void Part1TestData(int gridSerialNumber, int power, string expected)
+        {
+            var coords = Day11.Part1Solver(new[] { gridSerialNumber.ToString() });
+            Assert.AreEqual(expected, coords);
+        }
+
+
+        [Test]
+        public void Part2Solution()
+        {
+            var result = Day11.Part2Solver(Helpers.GetInputLines(11));
+            Assert.AreEqual(0, result);
+        }
+
+    }
+}
