f6da20f61ee124f776eeec6758b52e2ce61a79e5
Mark Heath <mark.heath@gmail.com>
2018-12-14 06:29:44 +0000

    day 14 part 1

---
+++ Solvers/Day14.cs
@@ -0,0 +1,37 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Solvers
+{
+    public static class Day14
+    {
+        public static string Part1Solver(int numberOfRecipes)
+        {
+            var elfPos = new[] { 0, 1 };
+            var recipes = new List<int>(numberOfRecipes+10) { 3, 7 };
+            for(int steps = 1; recipes.Count < numberOfRecipes + 10; steps++)
+            {
+                var newRecipe = recipes[elfPos[0]] + recipes[elfPos[1]];
+                if (newRecipe < 10)
+                    recipes.Add(newRecipe);
+                else
+                {
+                    recipes.Add(newRecipe / 10);
+                    recipes.Add(newRecipe % 10);
+                }
+                elfPos[0] = (elfPos[0] + 1 + recipes[elfPos[0]]) % recipes.Count;
+                elfPos[1] = (elfPos[1] + 1 + recipes[elfPos[1]]) % recipes.Count;
+            }
+            return recipes.Skip(numberOfRecipes).Take(10).ToDelimitedString("");
+        }
+
+        public static string Part2Solver(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/Day14Tests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Solvers;
+
+namespace Tests
+{
+    public class Day14Tests
+    {
+        private const string testData = @"";
+
+        [TestCase(9, "5158916779")]
+        [TestCase(5, "0124515891")]
+        [TestCase(18, "9251071085")]
+        [TestCase(2018, "5941429882")]
+        public void Part1TestData(int recipes, string expected)
+        {
+            var result = Day14.Part1Solver(recipes);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Part1Solution()
+        {
+            var result = Day14.Part1Solver(702831);
+
+            Assert.AreEqual("1132413111", result);
+        }
+
+        [Test]
+        public void Part2TestData()
+        {
+            var result = Day14.Part2Solver(testData.Split("\r\n"));
+
+            Assert.AreEqual("", result);
+        }
+
+        [Test]
+        public void Part2Solution()
+        {
+            var result = Day14.Part2Solver(Helpers.GetInputLines(14));
+
+            Assert.AreEqual("", result);
+        }
+    }
+}
